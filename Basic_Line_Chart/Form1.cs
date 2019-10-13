cc01374544be8b88a0c673a31d23512a998b08af
Fakhruddin90 <dinmsa@gmail.com>
2019-10-14 00:17:35 +0800

    first commit

---
+++ Basic_Line_Chart/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Line_Chart
+{
+    public partial class Form1 : Form
+    {
+        
+
+        private void InitializeComponent()
+        {
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cartesianChart1.Location = new System.Drawing.Point(13, 13);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(804, 501);
+            this.cartesianChart1.TabIndex = 0;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(829, 526);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
